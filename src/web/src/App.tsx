6e04ccb42ad6f9d66115e81d98b1bed8dccb9a34
JKLogos <knotter.1@osu.edu>
2024-03-22 13:09:34 -0400

    test test

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -3,10 +3,9 @@ import './App.css';
 function App() {
   return(
     <>
-      <div className='bg-gray-500'> Hello, World!</div>
+      <div className='bg-gray-500'>Hello World</div>
     </>
     );
   }
-
-
+  
   export default App;
